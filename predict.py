7391cc693ae91fd3f0b312701691af1d0af214ef
mosta <mostafakhaled835@outlook.com>
2023-03-27 23:45:17 +0300

    Finalizing the main requirements

--- predict.py
+++ predict.py
@@ -4,6 +4,8 @@ import torch
 from  lightningmodule import FashionMNISTDataClassifier
 from datamodule import FashionMNISTDataModule
 
+import matplotlib.pyplot as plt
+
 import hydra
 from omegaconf import DictConfig
 
@@ -17,11 +19,23 @@ def main(cfg: DictConfig):
 
     img, label = dm.get_test_image()
 
+    label = torch.tensor(label) if not isinstance(label, torch.Tensor) else label
+
     # Calculate the class probabilities (softmax) for img
     proba = torch.exp(model(img))
 
-    print(proba)
-
+    # Plot the image and probabilities
+    desc = ['T-shirt/top','Trouser','Pullover','Dress','Coat','Sandal','Shirt','Sneaker','Bag','Ankle Boot']
+    fig, (ax1, ax2) =  plt.subplots(figsize=(13, 6), nrows=1, ncols=2)
+    ax1.axis('off')
+    ax1.imshow(img.squeeze().reshape(28, 28))
+    ax1.set_title(desc[label.item()])
+    ax2.bar(range(10), proba.detach().cpu().numpy().squeeze())
+    ax2.set_xticks(range(10))
+    ax2.set_xticklabels(desc, size='small')
+    ax2.set_title('Predicted Probabilities')
+    plt.tight_layout()
+    plt.show()
 
 
 main()
