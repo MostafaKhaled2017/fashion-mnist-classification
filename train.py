1d27d68d409640401a31ed7c310e5bc8c978725a
mosta <mostafakhaled835@outlook.com>
2023-03-27 16:15:43 +0300

    Finalizing the Jupyter Notebook

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ from torchmetrics import Accuracy
 from pytorch_lightning.callbacks.progress import TQDMProgressBar
 from pytorch_lightning.loggers import CSVLogger
 
-#Data Module class
+#Data Module Class
 class FashionMNISTDataModule(pl.LightningDataModule):
     def __init__(self, data_dir: str = 'F_MNIST_data'):
         super().__init__()
@@ -55,4 +55,95 @@ class FashionMNISTDataModule(pl.LightningDataModule):
         return test_dl
     
 
-# Lightning Module
+#Lightning Model Class
+class FashionMNISTDataClassifier(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(OrderedDict([('fc1', nn.Linear(784, 128)),
+                                                ('relu1', nn.ReLU()),
+                                                ('drop1', nn.Dropout(0.25)),
+                                                ('fc2', nn.Linear(128, 64)),
+                                                ('relu2', nn.ReLU()),
+                                                ('drop1', nn.Dropout(0.25)),
+                                                ('output', nn.Linear(64, 10)),
+                                                ('logsoftmax', nn.LogSoftmax(dim=1))]))
+        self.loss_fn = nn.NLLLoss()
+
+        self.val_accuracy = Accuracy(task='multiclass', num_classes=10)
+        self.test_accuracy = Accuracy(task='multiclass', num_classes=10)
+
+        #Will be used for storing the average losses after each epoch
+        self.train_losses =[]
+        self.test_losses = []
+
+        #Will be used for storing the losses after each step
+        self.temp_training_losses = []
+        self.temp_validation_losses = []
+
+        
+    def forward(self, x):
+        x = self.network(x)
+        return x
+    
+    def training_step(self, batch, batch_idx):
+        images, labels = batch
+        images = images.view(images.shape[0], -1)
+        out = self.forward(images)
+        loss = self.loss_fn(out, labels)
+        self.log('train_loss', loss, prog_bar=True)
+        self.temp_training_losses.append(loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        images, labels = batch
+        images = images.view(images.shape[0], -1)
+        out = self.forward(images)
+        loss = self.loss_fn(out, labels)
+        preds = torch.argmax(out, dim=1)
+        self.val_accuracy.update(preds, labels)
+
+        self.log('val_loss', loss, prog_bar=True)
+        self.log('val_acc', self.val_accuracy, prog_bar=True)
+
+        self.temp_validation_losses.append(loss)
+    
+    def test_step(self, batch, batch_idx):
+        images, labels = batch
+        images = images.view(images.shape[0], -1)
+        out = self.forward(images)
+        loss = self.loss_fn(out, labels)
+        preds = torch.argmax(out, dim=1)
+        self.test_accuracy.update(preds, labels)
+        
+        self.log('test_loss', loss, prog_bar=True)
+        self.log('test_acc', self.test_accuracy, prog_bar=True)
+    
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=0.003)
+        return optimizer
+    
+    def on_train_epoch_end(self, *args, **kwargs):        
+        train_loss_mean = torch.stack(self.temp_training_losses).mean()
+        self.temp_training_losses = []
+        self.train_losses.append(train_loss_mean)
+
+
+    def on_validation_epoch_end(self, *arg, **kwargs):
+        val_loss_mean = torch.stack(self.temp_validation_losses).mean()
+        self.temp_validation_losses = []
+        self.test_losses.append(val_loss_mean)
+
+    def plot_losses(self):
+        train_losses_tensor = torch.tensor(self.train_losses, dtype=torch.float32)
+        test_losses_tensor = torch.tensor(self.test_losses, dtype=torch.float32)
+        
+        plt.plot(train_losses_tensor.detach().numpy(), label='train-loss')
+        plt.plot(test_losses_tensor.detach().numpy(), label='val-loss')
+        plt.legend()
+        plt.show()
+
+
+#Initializing the data module and the model
+dm = FashionMNISTDataModule()
+model = FashionMNISTDataClassifier()
+
